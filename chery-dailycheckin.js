2f8a09567f22ace4964d1b530b2db8655468bb23
0x30B56 <381445352@qq.com>
2023-02-09 15:32:02 +0800

    更新 chery-dailycheckin.js

--- chery-dailycheckin.js
+++ chery-dailycheckin.js
@@ -1,43 +1,49 @@
-const axios = require("axios")
-const schedule = require("node-schedule")
+以下是用Quantumult X实现的自动获取Cookies并签到脚本：
 
-const config = {
-        headers: {}
+```
+const loginUrl = "https://club.qirui.com/v2/api/?login";
+const signinUrl = "https://club.qirui.com/signin";
+
+// 替换为您的奇瑞车主俱乐部账号密码
+const loginData = {
+  username: "15613863036",
+  password: "Huxichen01"
+};
+
+// 登录奇瑞车主俱乐部
+$httpClient.post(loginUrl, function(error, response, data) {
+  if (error) {
+    console.log(error);
+    $done();
+  } else {
+    var cookies = response.headers["Set-Cookie"];
+    signin(cookies);
+  }
+}, loginData);
+
+// 签到奇瑞车主俱乐部
+function signin(cookies) {
+  $httpClient.get(signinUrl, function(error, response, data) {
+    if (error) {
+      console.log(error);
+      $done();
+    } else {
+      var result = JSON.parse(data);
+      if (result.errno === 0) {
+        console.log("签到成功，获得" + result.data.reward + "奖励");
+      } else {
+        console.log("今天已签到，无法获得奖励");
+      }
+      $done();
+    }
+  }, {
+    headers: {
+      Cookie: cookies
+    }
+  });
 }
+```
 
-const user = "" //账号
-const pass = ""        //密码
+您需要替换代码中的奇瑞车主俱乐部账号密码，然后在Quantumult X的脚本编辑器中输入上面的代码，并保存后运行它。
 
-
-async function sign() {
-        let re = ""
-
-        re = await axios.get("https://cheryclub.mychery.com/ump/cherypc/indexPC/index", )
-
-        let formToken = re.data.match(/name="formToken" value="(.*?)" \/>/)[1];
-        console.log(formToken);
-
-        config.headers.Cookie = re.headers['set-cookie'][0]
-
-        re = await axios.post("https://cheryclub.mychery.com/ump/cherypc/pcLogin/checkLogin",
-                `user=${user}&password=${pass}`, config)
-        console.log(re.data);
-
-        re = await axios.post("https://cheryclub.mychery.com/ump/cherypc/indexPC/addSign",
-                "formToken=" + formToken, config)
-        console.log(re.data);
-
-        return re.data.msg == "succ" ? true : false
-}
-
-async function start() {
-        for (let i = 0; i < 3; i++) {
-                if (await sign()) break
-        }
-}
-
-schedule.scheduleJob('0 0 8 * * *', () => {
-        start()
-})
-
-console.log(new Date + "脚本启动成功,每天8点定时签到!")
+希望这个脚本能帮助您。
