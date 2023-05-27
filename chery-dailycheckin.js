5933e069d2ef24e4e5e84e786ec593c4fad5bded
0x30B56 <381445352@qq.com>
2023-05-27 08:57:12 +0800

    更新 chery-dailycheckin.sgmodule

--- chery-dailycheckin.js
+++ chery-dailycheckin.js
@@ -1,42 +1,8 @@
+#!name=New Bing for other browsers
+#!desc=Unlock browser restrictions for new bing AI search.
 
-const loginUrl = "https://club.qirui.com/v2/api/?login";
-const signinUrl = "https://hybrid-sapp.chery.cn/package-mine/pages/sign-in/sign-in";
+[Header Rewrite]
+^https:\/\/www\.bing\.com\/(search|new) header-replace-regex User-Agent \w+\/[\d\.]+$ "AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0"
 
-// 替换为您的奇瑞车主俱乐部账号密码
-const loginData = {
-  username: "",
-  password: ""
-};
-
-// 登录奇瑞车主俱乐部
-$httpClient.post(loginUrl, function(error, response, data) {
-  if (error) {
-    console.log(error);
-    $done();
-  } else {
-    var cookies = response.headers["Set-Cookie"];
-    signin(cookies);
-  }
-}, loginData);
-
-// 签到奇瑞车主俱乐部
-function signin(cookies) {
-  $httpClient.get(signinUrl, function(error, response, data) {
-    if (error) {
-      console.log(error);
-      $done();
-    } else {
-      var result = JSON.parse(data);
-      if (result.errno === 0) {
-        console.log("签到成功，获得" + result.data.reward + "奖励");
-      } else {
-        console.log("今天已签到，无法获得奖励");
-      }
-      $done();
-    }
-  }, {
-    headers: {
-      Cookie: cookies
-    }
-  });
-}
+[MITM]
+hostname = %APPEND% www.bing.com
