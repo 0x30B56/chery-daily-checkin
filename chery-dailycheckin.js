e06d533e41833e8af012f7c04066a2cbac22b9fe
0x30B56 <381445352@qq.com>
2023-05-27 09:03:21 +0800

    更新 chery-dailycheckin.sgmodule

--- chery-dailycheckin.js
+++ chery-dailycheckin.js
@@ -2,7 +2,7 @@
 #!desc=Unlock browser restrictions for new bing AI search.
 
 [Header Rewrite]
-^https:\/\/hybrid-sapp\.chery\.cn\/\/package-mine\/pages\/sign-in\/sign-in header-replace-regex User-Agent \w+\/[\d\.]+$ "AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0"
+^https:\/\/hybrid-sapp\.chery\.cn\/\/package-mine\/pages\/sign-in\/sign-in header-replace-regex User-Agent \w+\/[\d\.]+$ ""
 
 [MITM]
 hostname = %APPEND% www.bing.com
