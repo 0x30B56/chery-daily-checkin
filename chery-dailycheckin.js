f55d17528f9159ce3c631cbcbf27692cce86b891
0x30B56 <381445352@qq.com>
2023-05-27 09:07:30 +0800

    Update and rename chery-dailycheckin.js to chery-dailycheckin.sgmodule

--- chery-dailycheckin.js
+++
@@ -1,8 +0,0 @@
-#!name=New Bing for other browsers
-#!desc=Unlock browser restrictions for new bing AI search.
-
-[Header Rewrite]
-^https:\/\/hybrid-sapp\.chery\.cn\/\/package-mine\/pages\/sign-in\/sign-in header-replace-regex User-Agent \w+\/[\d\.]+$ ""
-
-[MITM]
-hostname = %APPEND% www.bing.com
